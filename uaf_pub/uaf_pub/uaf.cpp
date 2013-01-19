50b154a335556bd468df7152c47921c8fb31e341
B <buherator@gmail.com>
2013-01-19 15:26:36 +0100

    just a simple demonstration of virtual functions

--- uaf_pub/uaf_pub/uaf.cpp
+++ uaf_pub/uaf_pub/uaf.cpp
@@ -5,18 +5,50 @@ using namespace std;
 class A{
 public: 
     void __construct__(){}
-
     virtual int vF1(){
         cout << "A::vF1" << endl;
         return 1; 
     }
+
+    void doSomething(){
+		cout << "A did something" << endl;
+        return;
+    }
+};
+
+class B: public A{
+public:
+    void __construct__(){}
+    virtual int vF1(){
+        cout << "B::vF1" << endl;
+        return 2; 
+    }
+    virtual int vF2(){
+        cout << "B::vF2" << endl;
+        return 2; 
+    }
+
+	void doSomething(){
+		cout << "B did something" << endl;
+        return;
+    }
 };
 
 int main(){
-    A *a=new A();
-    a->vF1();
-    free(a);
-    cout << "a freed" << endl;
-    a->vF1();
+    A *arr[10];
+
+	for (int i=0;i<10;i++){
+		if (i%2==0){
+			arr[i]=new A();
+		}else{
+			arr[i]=new B();
+		}
+	}
+
+	for (int i=0;i<10;i++){
+		arr[i]->vF1();
+		arr[i]->doSomething();
+	}
+
     return 0;
 }
