94362b2a6449069529d3cbc9a8f88107de8cd606
B <buherator@gmail.com>
2013-01-19 18:11:44 +0100

    letting control over the initial allocations

--- uaf_pub/uaf_pub/uaf.cpp
+++ uaf_pub/uaf_pub/uaf.cpp
@@ -35,15 +35,51 @@ public:
 };
 
 int main(){
+	int num0;
+	int num;
+	cout << "Number of initial allocations: ";
+	cin >> num0;
+	A **arr0=(A**)malloc(sizeof(A*)*num0);
+
+	for(int i=0;i<num0;i++){
+		arr0[i]=new A();
+		//printf("tmp%d at %p\n",i,arr0[i]);
+	} 
+	
 	A *a=new A();
+	
 	a->vF1();
+	
 	int size=sizeof(A);
 	
 	printf("Address of a: %p - Size: %d \n",a,size);
-	free(a);
+	cout << "Number of frees: ";
+	cin >> num;
 
-	void *p=malloc(size);
-	printf("Address of p: %p - Size: %d \n",p,size);
+	for(int i=0;i<num0 || i<num;i++){
+		free(arr0[i]);
+		//printf("tmp%d freed\n",i);
+	}
+
+	free(a);
+	cout << "a freed" << endl;
+
+	
+	cout << "Number of allocations: ";
+	cin >> num;
+	void **arr=(void**)malloc(size*num);
+	for(int i=0;i<num;i++){
+		arr[i]=malloc(size);
+		memset(arr[i],0x41,size);
+		printf("%d. item allocated to: %p \n",i,arr[i]);
+		
+		if (arr[i]==a){
+			cout << "Memory reallocated! " << endl;
+			return 1;
+		}
+	}
+
+	a->vF1();
 
 	return 0;
 }
